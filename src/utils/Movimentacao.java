18b69d4b3e5efff7441ddd1a2bc001dd0f2dfded
GustavoCarbono <gustavo.s.c.820@gmail.com>
2025-09-24 19:23:55 -0300

    Ajuste no tempo de movimentação

--- src/utils/Movimentacao.java
+++ src/utils/Movimentacao.java
@@ -43,7 +43,7 @@ public class Movimentacao {
 		}
 		int passos[] = {numRdm};
 		
-		Timer timer = new Timer(10, null);
+		Timer timer = new Timer(400, null);
 	    timer.addActionListener(e -> {
 	        if (passos[0] <= 0) {
 	            ((Timer) e.getSource()).stop();
