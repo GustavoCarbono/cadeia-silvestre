8436da746099d678916a196e57943a9634be3ee4
GustavoCarbono <gustavo.s.c.820@gmail.com>
2025-10-09 07:50:11 -0300

    Fazendo lista Presa

--- src/utils/ListaDePresas.java
+++ src/utils/ListaDePresas.java
@@ -1,20 +1,33 @@
 package utils;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import model.DAO;
+import model.PredacaoDAO;
 import partida.Animal;
+import partida.Jogador;
 import partida.Partida;
 
 public class ListaDePresas {
 	public List<Animal> getListaPresas(Partida partida, DAO dao) {
-		Map<String, List<String>> PresasAnimal = new HashMap<>();
 		List<Animal> listaAnimal = new ArrayList<>();
-		List<String> presasUnicas = new ArrayList<>();
-		List<String> presasComum = new ArrayList<>();
-		
+		for(Jogador jogador : partida.getJogadores()) {
+			Animal animal = jogador.getAnimal();
+			List<String> selecionado = new ArrayList<>();
+			for(PredacaoDAO presa : dao.buscarPresa(animal.getNome())) {
+				selecionado.add(presa.getNomePresa());
+			}
+			Collections.shuffle(selecionado);
+			
+			for(int i=0; i<6; i++) {
+				listaAnimal.add(new Animal(selecionado.get(i), dao.buscarAnimal(selecionado.get(i)).getImg());
+				System.out.println(listaAnimal.get(i));
+			}
+		}
+		return listaAnimal;
 	}
 }
